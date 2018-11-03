74285d1c377919e4ff75a74b800c5a2bbc996bc4
Andrew Lu <andrew,yunxiang@gmail.com>
2018-11-02 23:39:59 -0700

    End of 11/2/2018 day commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto/DepotFacingAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto/DepotFacingAuto.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode.opmodes.Auto;
 
+import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.opmodes.AutoOpMode;
 
-@Autonomous(name = "testLand", group = "test")
-public class AutoTestLand extends AutoOpMode {
+@Autonomous
+public class DepotFacingAuto extends AutoOpMode {
     @Override
     protected void setup() {
         robot.latch.lock();
@@ -14,5 +15,11 @@ public class AutoTestLand extends AutoOpMode {
     @Override
     protected void run() {
         land();
+        robot.drive.setVelocity(new Pose2d(0.5,0,0));
+        robot.sleep(1.15);
+        robot.drive.setVelocity(new Pose2d(0,-0.5,0));
+        robot.sleep(1.35);
+        robot.drive.stop();
+        sample();
     }
 }
