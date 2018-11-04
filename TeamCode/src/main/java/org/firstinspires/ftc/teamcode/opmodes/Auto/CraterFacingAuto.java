0a0ac8b779e37452f64b1330c4aff6e1dd5ea196
Andrew Lu <andrew,yunxiang@gmail.com>
2018-11-03 19:45:57 -0700

    End of 11/3/2018 day commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto/CraterFacingAuto.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.opmodes.Auto;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.opmodes.AutoOpMode;
+
+@Autonomous
+public class CraterFacingAuto extends AutoOpMode {
+    @Override
+    protected void setup() {
+        robot.latch.lock();
+        robot.depot.raise();
+    }
+
+    @Override
+    protected void run() {
+        land();
+        driveToSample();
+        sample();
+        robot.drive.enableHeadingCorrection(-3*Math.PI/4);
+        while (robot.drive.isMaintainHeading() && opModeIsActive()) {
+            idle();
+        }
+        robot.drive.setVelocity(new Pose2d(0,-0.5,0));
+        robot.sleep(1.5);
+        robot.drive.setHeading(3*Math.PI/4);
+        robot.drive.setVelocity(new Pose2d(0,0.3,0));
+        robot.sleep(0.5);
+        robot.drive.stop();
+        claim();
+        park();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoOpMode.java
@@ -1,19 +1,25 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.util.NanoClock;
 import com.disnodeteam.dogecv.CameraViewDisplay;
 import com.disnodeteam.dogecv.DogeCV;
 import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
 import com.disnodeteam.dogecv.detectors.roverrukus.SamplingOrderDetector;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Subsystems.FirstRobot;
 
 /**
  * Standard autonomous op mode with common functions
  */
 public abstract class AutoOpMode extends LinearOpMode {
-    public static final double SAMPLING_READ_TIMEOUT = 5; // seconds
+    protected NanoClock clock;
+
+    protected FtcDashboard dashboard;
+    protected Telemetry dashboardTelemetry;
 
     protected FirstRobot robot;
 
@@ -31,6 +37,13 @@ public abstract class AutoOpMode extends LinearOpMode {
         detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
         detector.useDefaults();
 
+        detector.alignSize = 200;
+
+        clock = NanoClock.system();
+
+        dashboard = FtcDashboard.getInstance();
+        dashboardTelemetry = dashboard.getTelemetry();
+
         setup();
 
         waitForStart();
@@ -44,7 +57,7 @@ public abstract class AutoOpMode extends LinearOpMode {
         robot.latch.unlock();
         robot.sleep(0.2);
         robot.latch.setWinchPower(-0.3);
-        robot.sleep(1.25);
+        robot.sleep(1.15);
         robot.latch.setWinchPower(0);
         robot.drive.enableHeadingCorrection(Math.PI / 2);
         while (robot.drive.isMaintainHeading() && opModeIsActive()) {
@@ -56,13 +69,46 @@ public abstract class AutoOpMode extends LinearOpMode {
         detector.enable();
         robot.sleep(1);
         robot.drive.setVelocity(new Pose2d(0,0.3,0));
+        double startTimeStamp = clock.seconds();
         while (!detector.getAligned() && !isStopRequested()) {
             idle();
         }
         robot.drive.stop();
+        double endTimeStamp = clock.seconds();
+        double deltaT = endTimeStamp - startTimeStamp;
         detector.disable();
         robot.drive.setVelocity(new Pose2d(0.5,0,0));
         robot.sleep(1);
         robot.drive.stop();
+        robot.drive.setVelocity(new Pose2d(-0.5,0,0));
+        robot.sleep(1);
+        robot.drive.stop();
+        dashboardTelemetry.addData("Delta T", deltaT);
+        dashboardTelemetry.update();
+        robot.drive.setVelocity(new Pose2d(0,0.3,0));
+        robot.sleep(7 - deltaT);
+        robot.drive.stop();
+    }
+
+    protected void driveToSample() {
+        robot.drive.setVelocity(new Pose2d(0.5,0,0));
+        robot.sleep(1.15);
+        robot.drive.setVelocity(new Pose2d(0,-0.5,0));
+        robot.sleep(1.45);
+        robot.drive.stop();
+    }
+
+    protected void park() {
+        robot.drive.setVelocity(new Pose2d(-1,0,0));
+        robot.sleep(4);
+        robot.drive.stop();
+    }
+
+    protected void claim() {
+        robot.drive.setVelocity(new Pose2d(0.5,0,0));
+        robot.sleep(3.5);
+        robot.drive.stop();
+        robot.depot.lower();
+        robot.sleep(0.5);
     }
 }
